10817c1feaebec57311a390598236e3d7f9e0ea9
Omar <omareegab@gmail.com>
2025-12-29 11:27:09 -0500

    postModel

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Profile extends Model
 {
@@ -28,5 +29,17 @@ class Profile extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /** @return HasMany  */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    /** @return HasMany  */
+    public function topLevelPost(): HasMany
+    {
+        return $this->hasMany(Post::class)->whereNull('parent_id');
     }
 }
